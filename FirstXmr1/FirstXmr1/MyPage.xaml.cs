b453fe79ca832cdf164626c576d4376214345874
epixiloga <hafbenmoussa@gmail.com>
2019-01-18 16:50:58 +0100

    calculate

---
+++ FirstXmr1/FirstXmr1/MyPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using FirstXmr1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstXmr1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+			
+
+
+			//IDialer dialer = DependencyService.Get<IDialer>();
+
+			//if (dialer != null)
+			//{
+			//	var boolVal = dialer.MakeCall("0103455634");
+			//	await DisplayAlert("totot", boolVal.ToString(), "OK");
+			//}
+
+
+				//var lUserApp = new Label()
+				//{
+				//	Text = "App M2I",
+				//	TextColor = Color.Red,
+				//	FontSize = 23,
+				//	VerticalOptions = LayoutOptions.CenterAndExpand,
+				//	HorizontalOptions = LayoutOptions.Center,
+				//};
+
+				//var lUserName = new Label()
+				//{
+				//	Text="Nom"
+				//};
+
+				//var EntryNom = new Entry()
+				//{
+				//	Placeholder="Entrez votre Nom"
+				//};
+
+				//StackLayout layout = new StackLayout();
+				//layout.Children.Add(lUserApp);
+				//layout.Children.Add(lUserName);
+				//layout.Children.Add(EntryNom);
+
+
+			}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			DisplayAlert("toto","Firstname: " + this.Firstname.Text.ToUpper() +
+												"\nLastname: "+ this.Lastame.Text.ToUpper()+
+												"\nAge: " + this.age.Text, "Valider");
+		}
+	}
+}
---
+++ FirstXmr1/FirstXmr1/PageCalculate.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstXmr1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageCalculate : ContentPage
+	{
+		public PageCalculate ()
+		{
+			InitializeComponent ();
+		}
+
+	
+		private void Button_Clicked_num(object sender, EventArgs e)
+		{
+			var myButton = (Button)sender;
+			this.num.Text += myButton.Text;
+
+			//return int.Parse(this.num.Text);
+		}
+
+		private void Button_Clicked_operator(object sender, EventArgs e)
+		{
+			var myButton = (Button)sender;
+			this.num.Text += myButton.Text;
+
+		}
+		private void Button_Clicked_calculate(object sender, EventArgs e)
+		{
+			var result = new DataTable().Compute(this.num.Text, "");
+			this.num.Text = result.ToString();
+		}
+
+		private void Button_Clicked_clear(object sender, EventArgs e)
+		{
+			this.num.Text = "0";
+		}
+	}
+}
